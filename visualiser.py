8247cc785a7759f92a7e7c25259ca4e14f0edd94
Nathan <ngoutcher1999@gmail.com>
2020-05-02 15:15:43 +0100

    Made program give app output

--- visualiser.py
+++ visualiser.py
@@ -5,95 +5,100 @@ Created on Thu Apr 30 12:23:09 2020
 @author: Nathan
 """
 import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+import matplotlib
 import random
+import tkinter as tk
+matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 
-def driver():
-    data = []
-    for i in range(100):
-        data.append(random.randint(0, 1000))
+class App(tk.Tk):
+    def __init__(self, parent):
+        tk.Tk.__init__(self, parent)
+        self.data = []
+        for i in range(100):
+            self.data.append(random.randint(0, 1000))
+
+        self.Fig = matplotlib.figure.Figure(figsize=(5,4),dpi=100)
+        self.FigSubPlot = self.Fig.add_subplot(111)
+        self.graph = self.FigSubPlot.bar(np.arange(len(self.data)), self.data, align='center', alpha=0.5)
+        self.canvas = FigureCanvasTkAgg(self.Fig, master=self)
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
     
-    plt.ion()
+        button = tk.Button(self, text="Start", command=self.start).pack(side=tk.TOP)
     
-    result = bubble_sort(data, plt)
-    data = result[0]
-    title = result[1]
+    def refresh_graph(self, data):
+        self.FigSubPlot.clear()
+        self.graph = self.FigSubPlot.bar(np.arange(len(data)), data, align='center', alpha=0.5)
+        self.canvas.draw()
+        
+    def start(self):
+        self.selection_sort(self.data)
+        
+    def bubble_sort(self, data):
+        title = "Bubble Sort"
+        n = len(data)
+        swap = False
     
-    plt.title(title)
-    plt.bar(np.arange(len(data)), data, align='center', alpha=0.5, color='g')
-    plt.draw()
-    plt.pause(0.001)
-    
-def plot_graph(data, graph, title):
-    graph.title(title)
-    graph.bar(np.arange(len(data)), data, align='center', alpha=0.5)
-    graph.draw()
-    graph.pause(0.001)
-    
-def bubble_sort(data, graph):
-    title = "Bubble Sort"
-    n = len(data)
-    swap = False
-    
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if data[j] > data[j+1]:
-                data[j], data[j+1] = data[j+1], data[j]
-                swap = True
+        for i in range(n):
+            for j in range(0, n-i-1):
+                if data[j] > data[j+1]:
+                    data[j], data[j+1] = data[j+1], data[j]
+                    swap = True
                 
-        if swap == False:
-            break
+            if swap == False:
+                break
         
-        plot_graph(data, graph, title)
-    return data, title
-
-def selection_sort(data, graph):
-    title = "Selection Sort"
-    for i in range(len(data)):
-        min_index = i
-        for j in range(i+1, len(data)):
-            if data[min_index] > data[j]:
-                min_index = j
-        
-        data[i], data[min_index] = data[min_index], data[i]
-        
-        plot_graph(data, graph, title)
-    return data, title
-
-def merge_sort(data, graph):
-    title = "Merge Sort"
-    if len(data) > 1:
-        mid = len(data)//2
-        l = data[:mid]
-        r = data[mid:]
-        
-        merge_sort(l, graph)
-        merge_sort(r, graph)
-        
-        i = j = k = 0
-        while i < len(l) and j < len(r):
-            if l[i] < r[j]:
+            self.refresh_graph(data)
+        return data
+    
+    def selection_sort(self, data):
+        title = "Selection Sort"
+        for i in range(len(data)):
+            min_index = i
+            for j in range(i+1, len(data)):
+                if data[min_index] > data[j]:
+                    min_index = j
+            
+            data[i], data[min_index] = data[min_index], data[i]
+            
+            self.refresh_graph(data)
+        return data
+    
+    def merge_sort(self, data):
+        title = "Merge Sort"
+        if len(data) > 1:
+            mid = len(data)//2
+            l = data[:mid]
+            r = data[mid:]
+            
+            self.merge_sort(l)
+            self.merge_sort(r)
+            
+            i = j = k = 0
+            while i < len(l) and j < len(r):
+                if l[i] < r[j]:
+                    data[k] = l[i]
+                    i += 1
+                else:
+                    data[k] = r[j]
+                    j += 1
+                k += 1
+                
+            while i < len(l):
                 data[k] = l[i]
                 i += 1
-            else:
+                k += 1
+                
+            while j < len(r):
                 data[k] = r[j]
                 j += 1
-            k += 1
-            
-        while i < len(l):
-            data[k] = l[i]
-            i += 1
-            k += 1
-            
-        while j < len(r):
-            data[k] = r[j]
-            j += 1
-            k += 1
-            
-        plot_graph(data, graph, title)
-    return data, title
-    
+                k += 1
+                
+            self.refresh_graph(data)
+        return data
+
+if __name__ == "__main__":
+    app = App(None)
+    app.mainloop()
